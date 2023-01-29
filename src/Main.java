f5a744700496f649dbf0c6622dd274c4c814ed19
cooluser <avzhykov@gmail.com>
2023-01-29 09:21:34 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Главный класс программы
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Запускается приложение
+        MyFrame myFrame = new MyFrame();
+    }
+
+}
---
+++ src/MyFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+/**
+ * Класс окна игрового поля
+ */
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        // Заголовок игры
+        setTitle("Игра Змейка");
+
+        // Границы расположения окна
+        setBounds(0,0,800,650);
+
+        // Создаем холст (панель), на котором все разместим
+        MyPanel myPanel = new MyPanel();
+        add(myPanel);
+
+        // Отображаем окно
+        setVisible(true);
+    }
+}
